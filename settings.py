2c34187844f242045387ad937626ed22a3dc112b
root <root@local.(none)>
2015-05-11 17:31:47 +0200

    repairs of failed rework

---
+++ settings.py
@@ -0,0 +1,9 @@
+# Directory for storing configuration files.
+CONF_DIR = '/etc/ipf/'
+
+# Directory for storing logs.
+LOG_DIR = '/var/log/'
+
+# Warning in the ippool.conf configuration file
+# for a recognition of a user defined ippool and IP blacklist.
+CONF_WARNING = '#CONFIGURATION UNDER THIS LINE WILL BE DELETED AT UPDATE'
